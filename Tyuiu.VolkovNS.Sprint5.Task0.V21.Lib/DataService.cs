2da11ee9ebbaf424c7c63f5663b4fb6c289b4541
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-23 10:25:39 +0300

    Task0

--- Tyuiu.VolkovNS.Sprint5.Task0.V21.Lib/DataService.cs
+++ Tyuiu.VolkovNS.Sprint5.Task0.V21.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.VolkovNS.Sprint5.Task0.V21.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Path.GetTempPath()}OutPutFileTask0.txt";
+            string path = $@"{Path.GetTempFileName()}OutPutFileTask0.txt";
             double y = (Math.Pow(x, 2) + 1)/Math.Pow(4*x*x-3, 0.5);
             y = Math.Round(y,2);
             File.WriteAllText(path,Convert.ToString(y));
