e1c7051771f96d192a9a304d8519e09a9df5f971
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-23 11:56:32 +0300

    Task4

--- Tyuiu.VolkovNS.Sprint5.Task4.V5.Lib/DataService.cs
+++ Tyuiu.VolkovNS.Sprint5.Task4.V5.Lib/DataService.cs
@@ -7,7 +7,8 @@ namespace Tyuiu.VolkovNS.Sprint5.Task4.V5.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double res = Math.Round(4.26 * Convert.ToDouble(strX) / Math.Sin(Convert.ToDouble(strX)), 3);
+            string strY = strX.Replace(".", ",");
+            double res = Math.Round(4.26 * Convert.ToDouble(strY) / Math.Sin(Convert.ToDouble(strY)), 3);
             return res;
         }
     }
