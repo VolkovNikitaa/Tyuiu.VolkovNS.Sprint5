cf87f78094bd65b5279cf7fb62b7ad4dd1883112
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-23 11:22:15 +0300

    Task3

--- Tyuiu.VolkovNS.Sprint5.Task3.V8.Lib/DataService.cs
+++ Tyuiu.VolkovNS.Sprint5.Task3.V8.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.VolkovNS.Sprint5.Task3.V8.Lib
             bool fileExists = fileinfo.Exists;
             if (fileExists)
                 File.Delete(path);
-            double res = (x * x + 1) / (3 * x + 4);
+            double res = Math.Round((x * x + 1) / (3 * x + 4),3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
             {
                 writer.Write(BitConverter.GetBytes(res));
