0e95a94d3e1f54ebcb66c5913f0f1bb46f65e2be
Nikita <Nikita@DESKTOP-BFU5003>
2025-11-23 10:19:45 +0300

    Task0

--- Tyuiu.VolkovNS.Sprint5.Task0.V21.Test/DataServiceTest.cs
+++ Tyuiu.VolkovNS.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.VolkovNS.Sprint5.Task0.V21.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Nikita\source\repos\Tyuiu.VolkovNS.Sprint5\Tyuiu.VolkovNS.Sprint5.Task0.V21\bin\Debug\OutPutFileTask0.txt";
+            string path = @"C:\Users\Nikita\AppData\Local\Temp\OutPutFileTask0.txt";
             FileInfo fileinfo = new FileInfo(path);
             bool fileExists = fileinfo.Exists;
             bool wait = true;
